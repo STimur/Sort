f4b87a8ce83e4d101d9fb5ca392eb792a2757627
Timur <timur.salakhetdinov@gmail.com>
2017-09-14 18:39:40 +0300

    amend sort of three elements

--- src/test/java/SortTest.java
+++ src/test/java/SortTest.java
@@ -36,6 +36,7 @@ public class SortTest {
         assertSorts(intList(2, 1, 3), intList(1, 2, 3));
         assertSorts(intList(2, 3, 1), intList(1, 2, 3));
         assertSorts(intList(3, 2, 1), intList(1, 2, 3));
+        assertSorts(intList(1, 3, 2), intList(1, 2, 3));
     }
 
     private List<Integer> sort(List<Integer> list) {
@@ -55,7 +56,7 @@ public class SortTest {
         }
         sorted.addAll(sort(l));
         sorted.add(m);
-        sorted.addAll(h);
+        sorted.addAll(sort(h));
 
         return sorted;
     }
